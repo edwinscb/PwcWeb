de1403aa59c16ea49ceabc341ed77cfe26a822ed
edwin <edwinsantiagocastrobuitrago@gmail.com>
2021-12-17 02:32:28 -0500

    config basic,creation {home ,login,404}

--- PwcWeb/src/app/app.component.spec.ts
+++ PwcWeb/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'PwcWeb'`, () => {
+  it(`should have as title 'angular-trials'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('PwcWeb');
+    expect(app.title).toEqual('angular-trials');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('PwcWeb app is running!');
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-trials app is running!');
   });
 });
